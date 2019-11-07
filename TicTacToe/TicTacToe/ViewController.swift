fceaaa2314881c2d4dfa493ddbb23cfb05e5b752
Gregory Keeley <gregkeeley@Gregorys-MBP.fios-router.home>
2019-11-06 21:14:01 -0500

    Updated lab with grid, and functioning buttons

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    @IBOutlet weak var gameButton1: GameButton!
+    @IBOutlet weak var gameButton2: GameButton!
+    @IBOutlet weak var gameButton3: GameButton!
+    @IBOutlet weak var gameButton4: GameButton!
+    @IBOutlet weak var gameButton5: GameButton!
+    @IBOutlet weak var gameButton6: GameButton!
+    @IBOutlet weak var gameButton7: GameButton!
+    @IBOutlet weak var gameButton8: GameButton!
+    
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    var activePlayer = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainLabel.text = "Welcome to Tic Tac Toe?"
+    }
+    func gameCheck(button: GameButton) -> Bool {
+    return true
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if (gameState[sender.tag] == 0) {
+            if activePlayer == 1 {
+                sender.setTitle("X", for: .normal)
+                activePlayer = 2
+                gameState[sender.tag] = 1
+                print(activePlayer)
+                print(gameState)
+                mainLabel.text = "Player 2s turn"
+            } else {
+                sender.setTitle("O", for: .normal)
+                activePlayer = 1
+                gameState[sender.tag] = 1
+                print(activePlayer)
+                print(gameState)
+                mainLabel.text = "Player 1s turn"
+            }
+        }
+    }
+  
+    
 }
-
